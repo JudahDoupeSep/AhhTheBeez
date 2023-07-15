a5d33b642800a04ac0ac186f566ecb13f457f2be
Clayton <cjdetke@yahoo.com>
2023-07-15 14:13:59 -0400

    Add cost to hivePurchaseButton

--- Assets/Scripts/hivePurchaseButton.cs
+++ Assets/Scripts/hivePurchaseButton.cs
@@ -6,18 +6,29 @@ public class hivePurchaseButton : MonoBehaviour
 {
     public GameObject hive;
     public Vector3[] newHivePositions;
+    public int cost = 30;
 
+    private honeyCounter totalHoney;
     private int nextPosition = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        totalHoney = GameObject.FindGameObjectWithTag("HiveMind").GetComponent<honeyCounter>();
+    }
 
     private void OnMouseDown()
     {
-        // TODO: spend honey
-        Instantiate(hive, transform.position, Quaternion.identity);
-        // TODO: make this button invisible until we have enough honey to purchase?
-        if (nextPosition < newHivePositions.Length)
+        if (totalHoney.totalHoney >= cost)
         {
-            transform.position = newHivePositions[nextPosition];
-            nextPosition++;
+            totalHoney.totalHoney -= cost;
+            Instantiate(hive, transform.position, Quaternion.identity);
+            // TODO: make this button invisible until we have enough honey to purchase?
+            if (nextPosition < newHivePositions.Length)
+            {
+                transform.position = newHivePositions[nextPosition];
+                nextPosition++;
+            }
         }
     }
 }
