da7b7386b1f31c13f22377aa62ac96fb9bbf7594
Andy Jang <andyjang0213@live.com>
2023-07-15 14:28:27 -0400

    Add queen bee purchase

---
+++ Assets/Scripts/queenBeePurchase.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class queenBeePurchase : MonoBehaviour
+{
+    public GameObject queenBee;
+    public GameObject hive;
+    private honeyCounter totalHoney;
+
+    public int price = 5;
+
+    private void OnMouseDown()
+    {
+        if (totalHoney.totalHoney > price)
+        {
+            totalHoney.totalHoney -= price;
+            GameObject newQueen = Instantiate(queenBee);
+            newQueen.GetComponent<queenBee>().setHive(hive);
+            Destroy(gameObject);
+        }
+    }
+
+    void Start()
+    {
+        GameObject controller = GameObject.FindGameObjectWithTag("HiveMind");
+        totalHoney = controller.GetComponent<honeyCounter>();
+    }
+}
