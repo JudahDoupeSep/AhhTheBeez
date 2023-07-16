1dee0a03884ebda3a3e2687904c67f49fe0a991f
jjdoupe <jjdoupe@sep.com>
2023-07-16 13:43:48 -0400

    price highlighting

--- Assets/Scripts/PriceDisplay.cs
+++ Assets/Scripts/PriceDisplay.cs
@@ -7,8 +7,14 @@ public class PriceDisplay : MonoBehaviour
     public TextMeshPro PriceText;
     public float Padding;
 
+    public Color TextColor;
+    public Color ErrorColor;
+    
+    private int _price;
+    
     public void UpdatePrice(int newPrice)
     {
+        _price = newPrice;
         PriceText.text = $"{newPrice}";
     }
     
@@ -30,6 +36,7 @@ public class PriceDisplay : MonoBehaviour
     void Update()
     {
         UpdateBackground();
+        UpdatePriceText();
     }
 
     void UpdateBackground()
@@ -37,5 +44,14 @@ public class PriceDisplay : MonoBehaviour
         var sprite = GetComponent<SpriteRenderer>();
         var rect = GetComponent<RectTransform>().rect;
         sprite.size = new Vector2(rect.width + Padding, rect.height + Padding);
+    }
+
+    void UpdatePriceText()
+    {
+        var color = FindObjectOfType<honeyCounter>().totalHoney < _price
+            ? ErrorColor
+            : TextColor;
+        PriceText.color = color;
+        PriceText.text = _price.ToString();
     }
 }
